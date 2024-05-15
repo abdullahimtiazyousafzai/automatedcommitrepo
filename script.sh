9254bb7198e7cb1c80693dcb01c5025b32cfe216
abdullahimtiazyousafzai <abdullahimtiazyousafzai@gmail.com>
2024-05-15 05:20:51 +0500

    update script

--- script.sh
+++ script.sh
@@ -1,3 +1,18 @@
+#!/bin/bash
+
+# Number of commits
+n=100
+
+# Loop to commit n times
+for ((i=1; i<=n; i++))
+do
+    echo -e "\nNow time: $(date)" >> README.md
+    git add README.md
+    git commit -m "Update README with current time (Commit $i of $n)"
+done
+
+# Push changes to origin main branch
+git push origin main
 echo -e "\nNow time: $(date)" >> README.md;
 git add README.md;
 git commit -m "Update README with current time";
